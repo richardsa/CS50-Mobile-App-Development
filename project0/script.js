82b7dd1eb9cbacd52c888f8e161ea84cca4d2a29
Richard Saldivar <rsaldiva@gmail.com>
2018-07-23 13:07:48 -0400

    working prototype for todo app

--- project0/script.js
+++ project0/script.js
@@ -11,12 +11,42 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
   const newItem = $('#todoInput').val();
-  console.log(newItem);
-  $("#todo-list").prepend("<li>" + newItem + "</li>");
-  $('#todoInput').val('');
+  if (newItem !== '') {
+    $("#todo-list").prepend('<li><input class="form-check-input finished" type="checkbox" value="' + newItem + '"> <label class="form-check-label" for="defaultCheck1">' + newItem + '</label> <span class="oi deleteItem" data-glyph="trash" title="trash" aria-hidden="true"></span></li>');
+    $('#todoInput').val('');
+    getCounts();
+  }
 }
 
-$('#todo-form').on('submit', function(e){
-    e.preventDefault();
-    newTodo();
+function getCounts(){
+  const itemCount = $('ul#todo-list li').length;
+  const uncheckedCount = $('#todo-list input:checkbox:not(:checked)').length;
+  $( "#item-count" ).text(itemCount);
+  $( "#unchecked-count" ).text(uncheckedCount);
+}
+
+$( document ).ready(function() {
+  /* listen for form submit */
+  $('#todo-form').on('submit', function(e){
+      e.preventDefault();
+      newTodo();
+  });
+
+  /* listen for check box click to indicate finished task */
+  $( '#todo-list' ).on( 'click', 'input[type="checkbox"]', function() {
+    if ($(this).is(':checked')) {
+      $(this).next('label').addClass( "finished-item" );
+    } else {
+      $(this).next('label').removeClass( "finished-item" );
+    }
+    getCounts();
+  });
+
+  /* listen for delete button click and remove item accordingly */
+  $( '#todo-list' ).on( 'click', '.deleteItem', function() {
+     $(this).parent().remove();
+     getCounts();
+  });
+
+/* document ready closing tag */
 });
