7f90bd817721b2dda7369e2b44c689fcf4bc9853
Richard Saldivar <rsaldiva@gmail.com>
2019-03-04 19:11:25 -0500

    add cover to search results

--- project2/Row.js
+++ project2/Row.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native'
 
 const styles = StyleSheet.create({
-  row: {padding: 20},
+  row: {padding: 20, flexDirection: "row", alignItems: 'center'},
+  image: {marginRight: 15, width: 50, height: 70}
+
 })
 
 const Row = props => (
   <TouchableOpacity
     style={styles.row}
     onPress={() => props.onSelectMovie(props)}>
+    <Image source={{uri: props.Poster}}
+      style={styles.image} />
     <Text>{props.Title} - {props.Year}</Text>
   </TouchableOpacity>
 )
